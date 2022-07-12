53b7a03de3ec351762adc1cc8394ec40489fc34a
elsy0111 <kpp0193@gmail.com>
2022-07-12 17:29:06 +0900

    newest

--- plot.py
+++ plot.py
@@ -4,10 +4,15 @@ import numpy as np
 import matplotlib
 matplotlib.rcParams['text.usetex'] = True
 
+# 上記のライブラリをインストールの上、Latexのインストールが必要です。(表示にLatexを用いている為)
+
 fig = plt.figure(figsize=(6,4))
 ax = Axes3D(fig)
+ax.set_xlabel(r'$x$')
+ax.set_ylabel(r'$y$')
+ax.set_zlabel(r'$z$')
 
-#define each vector----
+# define each vector----
 xa = [0, 1]
 ya = [0, 3]
 za = [0, 5]
@@ -25,11 +30,11 @@ yf = [0,-3]
 zf = [0,3]
 #---------------------
 
+# 上記のベクトル(O原点)をプロット
 ax.plot(xa, ya, za, "-", color="#ff0000", ms=6, mew=0.5,label = r"$\vec{a}$")
 ax.plot(xb, yb, zb, "-", color="#00ff00", ms=6, mew=0.5,label = r"$\vec{b}$")
 ax.plot(xc, yc, zc, "-", color="#0016ff", ms=6, mew=0.5,label = r"$\vec{c}$")
 ax.plot(xf, yf, zf, "-", color="#f400ff", ms=12, mew=0.5,label = r"$\vec{f}$")
-
 
 def plot_plane(ax, param, xrange, yrange, pcolor="blue", alpha=0.15):
 
@@ -47,34 +52,29 @@ def plot_plane(ax, param, xrange, yrange, pcolor="blue", alpha=0.15):
 # a,b,c,dを設定
 param = [-22, -6, 8, 0]
 
-# x,y軸の範囲
+# 平面のx,y軸の範囲
 xr = [0, 3.1]
 yr = [-2, 3.1]
 
 # 平面をプロット
 plot_plane(ax, param, xr, yr)
 
-"""
-linesux = [1,3]
-linesuy = [3,1]
-linesuz = [5,9]
-
-
-linetux = [1,-1]
-linetuy = [3,-3]
-linetuz = [5,-5]
-
-
-ax.plot(linesux, linesuy, linesuz, "--", color="#f4ff00", ms=12, mew=0.5)
-ax.plot(linetux, linetuy, linetuz, "--", color="#ff8500", ms=12, mew=0.5)
-"""
-
-ax.set_xlabel(r'$x$')
-ax.set_ylabel(r'$y$')
-ax.set_zlabel(r'$z$')
+# ラベル(凡例)のプロット
 plt.legend(fontsize=12, loc='upper right', bbox_to_anchor=(1, 0.9))
 
-for i in range(180):
-    ax.view_init(elev=10, azim=(i)*2)
-    plt.savefig("images/"+str(i+1)+".png",dpi = 300)
-    print("saved"+str(i+1))
+# #png export-----------------------
+# for i in range(180):
+#     ax.view_init(elev=10, azim=(i)*2)
+#     plt.savefig("images/"+str(i+1)+".png")
+#     print("saved"+str(i+1))
+# #---------------------------------
+
+# #svg export-----------------------
+# for i in range(180):
+#     ax.view_init(elev=10, azim=(i)*2)
+#     plt.savefig("images_svg/"+str(i+1)+".svg")
+#     print("saved"+str(i+1))
+# #---------------------------------
+
+# 表示
+plt.show()
